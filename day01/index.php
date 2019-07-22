0c94328643851b9f832ab019cf15223464dbc9dd
annachen88 <32476353+annachen88@users.noreply.github.com>
2019-07-22 17:09:56 +0800

    Day01_html basic

---
+++ day01/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Website</title>
+</head>
+<body>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ol>
+    
+    <img src="https://picsum.photos/id/1059/300/500" alt="room photo">
+    <img src="./boy.jpg" alt="boy photo" width="600px">
+    <a href="https://www.google.com" target="_blank">here for google</a>
+    <a href="./about.php">here for about page</a>
+    <!-- control ?註解 -->
+    <h1>壽司郎</h1>
+    <h2>壽司郎</h2>
+    <hr>
+    <!-- lorem20+tab鍵 -->
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem asperiores qui corporis consequuntur, natus debitis laboriosam consectetur necessitatibus fuga minus.</p>
+
+</body>
+</html>
