9c5e78f416651f5bdbb91b03261f716b2df4ebf2
annachen88 <32476353+annachen88@users.noreply.github.com>
2019-07-25 16:09:45 +0800

    php loop

--- day04/example01.php
+++ day04/example01.php
@@ -36,5 +36,68 @@
         //echo "不足60分:C";
         $garde="C";
     }
-    echo "你的等級:".$garde;
+    echo "你的等級:".$garde."<br>";
+
+    $total = 0;
+    for($index = 1; $index<=10; $index++){
+        // echo $index."<br>";
+        //$total = $total + $index;
+        $total += $index;
+    }
+    echo $total;
+    echo "<br>";
+
+    $sum = 0;
+    for($index = 3; $index<=103 ; $index+=2){
+        $sum += $index;
+    }
+    echo $sum;
+    echo "<br>";
+
+    $text = "";
+    for($index = 1; $index<=10 ; $index++){
+        if($index===3 || $index===5){
+            continue;//跳過
+        }else{
+            $text .= $index;
+        }
+    } 
+     echo $text;
+    echo "<br>";
+    
+    $j=1;
+    $sum=0;
+    while($j<=10){
+        $sum += $j;
+        $j++;
+    }
+    echo $j."<br>";
+    
+    //1~100中總和所有4的倍數的數字。
+    $total=0;
+    for($number=0;$number<=100;$number++){
+        if($number%4===0){
+            $total += $number;
+        }else{
+            continue;
+        }
+    }
+    echo $total;
+    echo "<br>";
+
+    //尋找1~50中是3的倍數，將前九個印出來，並且找到第九個就停止尋找。
+    $count=1;
+    for($number=1;$number<=50;$number++){
+        if($count<=9){
+            if($number%3===0){
+                echo "第".$count."個3的倍數:".$number."<br>";
+                $count++;
+            }else{
+                continue;
+            }
+        }else{
+            break;
+        }
+        
+    }
 ?>
---
+++ day04/example02.php
@@ -0,0 +1,5 @@
+<?php
+    $colors = array("Red","Black","Pink");
+    echo $colors[2];
+    //陣列
+?>
