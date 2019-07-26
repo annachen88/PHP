5cb88e5d7eb7be65bbaff4e09fc1c0a73c174559
annachen88 <32476353+annachen88@users.noreply.github.com>
2019-07-26 14:36:55 +0800

    php html fruits array practice

---
+++ day05/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="result.php" method="POST">
+        <h3>水果字典</h3>
+        <p>
+            <label for="word">輸入要查詢的水果名</label>
+            <input type="text" name="word" id="word">
+        </p>
+        <input type="submit" value="查詢">
+    </form>
+</body>
+</html>
