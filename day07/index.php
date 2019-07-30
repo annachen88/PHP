bbd122d06aed182a0e16d74585138afb9fb9b5cb
annachen88 <32476353+annachen88@users.noreply.github.com>
2019-07-30 15:41:33 +0800

    php date() mktime()

--- day07/index.php
+++ day07/index.php
@@ -21,6 +21,28 @@
     $urlArr = explode(".",$url);
     print_r($urlArr);
     echo "<br>";
-
+    
+     //date
+     echo date("Y/m/d H:i:s");
+     echo "<br>";
+     //今天是民國108年07月30日
+     //現在是下午14點59分04秒
+     $timeSlot = (date("a")==="am")? "早上" : "下午";
+     echo "今天是民國".(date("Y")-1911)."年".date("m")."月".date("d")."日<br>";
+     echo "現在是".$timeSlot.date("H")."點".date("i")."分".date("s")."秒<br>";
+     
+     //時間(秒)mktime(時，分，秒，月，日，年)
+     $targetTimestamp = mktime(0,0,0,12,31,2019);
+     echo $targetTimestamp."<br>";
+     //推回日期時間格式
+     $dateTime = date("Y-m-d H:i:s" , $targetTimestamp);
+     echo $dateTime."<br>";
+     //倒數日秒數
+     $eventDate = mktime(0,0,0,01,01,2020);
+     $currentTime = date('U'); //目前絕對秒數
+     $leftTime = $eventDate - $currentTime;
+     //秒 => 天
+     $leftTime = floor($leftTime/60/60/24); //floor小數後捨去
+     echo $leftTime;
    
 ?>
